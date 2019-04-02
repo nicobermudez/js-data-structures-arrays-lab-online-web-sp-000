4dcc84c755942668bd6ee84613658e39901bad04
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-02 22:34:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function prependDriver(name) {
 }
 
 function removeLastDriver() {
-  drivers.slice(0, drivers.length - 1)
+  return drivers.slice(0, drivers.length - 1)
 }
 function removeFirstDriver() {
 
